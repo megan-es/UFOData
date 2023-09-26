bd07f965b7315481c13d96a6602b010f9d8496a3
megan-es <121473079+megan-es@users.noreply.github.com>
2023-09-26 09:02:20 -0700

    first commit

---
+++ mappings.py
@@ -0,0 +1,466 @@
+state_province_to_country = {
+    # United States
+    'al': 'us',
+    'ak': 'us',
+    'az': 'us',
+    'ar': 'us',
+    'ca': 'us',
+    'co': 'us',
+    'ct': 'us',
+    'de': 'us',
+    'fl': 'us',
+    'ga': 'us',
+    'hi': 'us',
+    'id': 'us',
+    'il': 'us',
+    'in': 'us',
+    'ia': 'us',
+    'ks': 'us',
+    'ky': 'us',
+    'la': 'us',
+    'me': 'us',
+    'md': 'us',
+    'ma': 'us',
+    'mi': 'us',
+    'mn': 'us',
+    'ms': 'us',
+    'mo': 'us',
+    'mt': 'us',
+    'ne': 'us',
+    'nv': 'us',
+    'nh': 'us',
+    'nj': 'us',
+    'nm': 'us',
+    'ny': 'us',
+    'nc': 'us',
+    'nd': 'us',
+    'oh': 'us',
+    'ok': 'us',
+    'or': 'us',
+    'pa': 'us',
+    'ri': 'us',
+    'sc': 'us',
+    'sd': 'us',
+    'tn': 'us',
+    'tx': 'us',
+    'ut': 'us',
+    'vt': 'us',
+    'va': 'us',
+    'wa': 'us',
+    'wv': 'us',
+    'wi': 'us',
+    'wy': 'us',
+    # Canada (abbreviations)
+    'ab': 'ca',
+    'bc': 'ca',
+    'mb': 'ca',
+    'nb': 'ca',
+    'nl': 'ca',
+    'nt': 'ca',
+    'ns': 'ca',
+    'nu': 'ca',
+    'on': 'ca',
+    'pe': 'ca',
+    'qc': 'ca',
+    'sk': 'ca',
+    'yt': 'ca',
+}
+
+country_full_names = {
+    "af": "afghanistan",
+    "al": "albania",
+    "dz": "algeria",
+    "ad": "andorra",
+    "ao": "angola",
+    "ag": "antigua and barbuda",
+    "ar": "argentina",
+    "am": "armenia",
+    "au": "australia",
+    "at": "austria",
+    "az": "azerbaijan",
+    "bs": "bahamas",
+    "bh": "bahrain",
+    "bd": "bangladesh",
+    "bb": "barbados",
+    "by": "belarus",
+    "be": "belgium",
+    "bz": "belize",
+    "bj": "benin",
+    "bm": "bermuda",
+    "bt": "bhutan",
+    "bo": "bolivia",
+    "ba": "bosnia and herzegovina",
+    "bw": "botswana",
+    "br": "brazil",
+    "bn": "brunei",
+    "bg": "bulgaria",
+    "bf": "burkina faso",
+    "bi": "burundi",
+    "bq": "bonaire, sint eustatius and saba",
+    "ci": "cote d'ivoire",
+    "cv": "cabo verde",
+    "kh": "cambodia",
+    "cm": "cameroon",
+    "ca": "canada",
+    "cf": "central african republic",
+    "td": "chad",
+    "cl": "chile",
+    "cn": "china",
+    "co": "colombia",
+    "km": "comoros",
+    "cg": "congo (congo-brazzaville)",
+    "cr": "costa rica",
+    "hr": "croatia",
+    "cu": "cuba",
+    "cy": "cyprus",
+    "cz": "czechia",
+    "cd": "democratic republic of the congo",
+    "dk": "denmark",
+    "dj": "djibouti",
+    "dm": "dominica",
+    "do": "dominican republic",
+    "ec": "ecuador",
+    "eg": "egypt",
+    "sv": "el salvador",
+    "gq": "equatorial guinea",
+    "er": "eritrea",
+    "ee": "estonia",
+    "sz": "eswatini",
+    "et": "ethiopia",
+    "fj": "fiji",
+    "fi": "finland",
+    "fr": "france",
+    "ga": "gabon",
+    "gm": "gambia",
+    "ge": "georgia",
+    "de": "germany",
+    "gh": "ghana",
+    "gr": "greece",
+    "gd": "grenada",
+    "gt": "guatemala",
+    "gn": "guinea",
+    "gw": "guinea-bissau",
+    "gy": "guyana",
+    "ht": "haiti",
+    "va": "holy see",
+    "hn": "honduras",
+    "hu": "hungary",
+    "is": "iceland",
+    "in": "india",
+    "id": "indonesia",
+    "ir": "iran",
+    "iq": "iraq",
+    "ie": "ireland",
+    "il": "israel",
+    "it": "italy",
+    "jm": "jamaica",
+    "jp": "japan",
+    "jo": "jordan",
+    "kz": "kazakhstan",
+    "ke": "kenya",
+    "ki": "kiribati",
+    "kw": "kuwait",
+    "kg": "kyrgyzstan",
+    "la": "laos",
+    "lv": "latvia",
+    "lb": "lebanon",
+    "ls": "lesotho",
+    "lr": "liberia",
+    "ly": "libya",
+    "li": "liechtenstein",
+    "lt": "lithuania",
+    "lu": "luxembourg",
+    "mg": "madagascar",
+    "mw": "malawi",
+    "my": "malaysia",
+    "mv": "maldives",
+    "ml": "mali",
+    "mt": "malta",
+    "mh": "marshall islands",
+    "mr": "mauritania",
+    "mu": "mauritius",
+    "mx": "mexico",
+    "fm": "micronesia",
+    "md": "moldova",
+    "mc": "monaco",
+    "mn": "mongolia",
+    "me": "montenegro",
+    "ma": "morocco",
+    "mz": "mozambique",
+    "mm": "myanmar",
+    "na": "namibia",
+    "nr": "nauru",
+    "np": "nepal",
+    "nl": "netherlands",
+    "nz": "new zealand",
+    "ni": "nicaragua",
+    "ne": "niger",
+    "ng": "nigeria",
+    "kp": "north korea",
+    "mk": "north macedonia",
+    "no": "norway",
+    "om": "oman",
+    "pk": "pakistan",
+    "pw": "palau",
+    "ps": "palestine state",
+    "pa": "panama",
+    "pg": "papua new guinea",
+    "py": "paraguay",
+    "pe": "peru",
+    "ph": "philippines",
+    "pl": "poland",
+    "pt": "portugal",
+    "qa": "qatar",
+    "ro": "romania",
+    "ru": "russia",
+    "rw": "rwanda",
+    "kn": "saint kitts and nevis",
+    "lc": "saint lucia",
+    "vc": "saint vincent and the grenadines",
+    "ws": "samoa",
+    "sm": "san marino",
+    "st": "sao tome and principe",
+    "sa": "saudi arabia",
+    "sn": "senegal",
+    "rs": "serbia",
+    "sc": "seychelles",
+    "sl": "sierra leone",
+    "sg": "singapore",
+    "sk": "slovakia",
+    "si": "slovenia",
+    "sb": "solomon islands",
+    "so": "somalia",
+    "za": "south africa",
+    "kr": "south korea",
+    "ss": "south sudan",
+    "es": "spain",
+    "lk": "sri lanka",
+    "sd": "sudan",
+    "sr": "suriname",
+    "se": "sweden",
+    "ch": "switzerland",
+    "sy": "syria",
+    "tj": "tajikistan",
+    "tz": "tanzania",
+    "th": "thailand",
+    "tl": "timor-leste",
+    "tg": "togo",
+    "to": "tonga",
+    "tt": "trinidad and tobago",
+    "tn": "tunisia",
+    "tr": "turkey",
+    "tm": "turkmenistan",
+    "tv": "tuvalu",
+    "ug": "uganda",
+    "ua": "ukraine",
+    "ae": "united arab emirates",
+    "gb": "united kingdom",
+    "us": "united states of america",
+    "uy": "uruguay",
+    "uz": "uzbekistan",
+    "vu": "vanuatu",
+    "ve": "venezuela",
+    "vn": "vietnam",
+    "ye": "yemen",
+    "zm": "zambia",
+    "zw": "zimbabwe"
+}
+
+country_code_mapping = {
+    'afg': 'af',  # Afghanistan
+    'alb': 'al',  # Albania
+    'dza': 'dz',  # Algeria
+    'and': 'ad',  # Andorra
+    'ago': 'ao',  # Angola
+    'atg': 'ag',  # Antigua and Barbuda
+    'arg': 'ar',  # Argentina
+    'arm': 'am',  # Armenia
+    'aus': 'au',  # Australia
+    'aut': 'at',  # Austria
+    'aze': 'az',  # Azerbaijan
+    'bhs': 'bs',  # Bahamas
+    'bhr': 'bh',  # Bahrain
+    'bgd': 'bd',  # Bangladesh
+    'brb': 'bb',  # Barbados
+    'blr': 'by',  # Belarus
+    'bel': 'be',  # Belgium
+    'blz': 'bz',  # Belize
+    'ben': 'bj',  # Benin
+    'btn': 'bt',  # Bhutan
+    'bol': 'bo',  # Bolivia
+    'bes': 'bq',  # Bonaire, Sint Eustatius and Saba
+    'bih': 'ba',  # Bosnia and Herzegovina
+    'bwa': 'bw',  # Botswana
+    'bra': 'br',  # Brazil
+    'brn': 'bn',  # Brunei
+    'bgr': 'bg',  # Bulgaria
+    'bfa': 'bf',  # Burkina Faso
+    'bdi': 'bi',  # Burundi
+    'bmu': 'bm',  # Bermuda
+    'cpv': 'cv',  # Cape Verde
+    'khm': 'kh',  # Cambodia
+    'cmr': 'cm',  # Cameroon
+    'can': 'ca',  # Canada
+    'caf': 'cf',  # Central African Republic
+    'tcd': 'td',  # Chad
+    'chl': 'cl',  # Chile
+    'chn': 'cn',  # China
+    'col': 'co',  # Colombia
+    'com': 'km',  # Comoros
+    'cog': 'cg',  # Congo
+    'cod': 'cd',  # Congo (DRC)
+    'cri': 'cr',  # Costa Rica
+    'civ': 'ci',  # Cote d'Ivoire
+    'hrv': 'hr',  # Croatia
+    'cub': 'cu',  # Cuba
+    'cyp': 'cy',  # Cyprus
+    'cze': 'cz',  # Czech Republic
+    'dnk': 'dk',  # Denmark
+    'dji': 'dj',  # Djibouti
+    'dma': 'dm',  # Dominica
+    'dom': 'do',  # Dominican Republic
+    'ecu': 'ec',  # Ecuador
+    'egy': 'eg',  # Egypt
+    'slv': 'sv',  # El Salvador
+    'gnq': 'gq',  # Equatorial Guinea
+    'eri': 'er',  # Eritrea
+    'est': 'ee',  # Estonia
+    'swz': 'sz',  # Eswatini
+    'eth': 'et',  # Ethiopia
+    'fji': 'fj',  # Fiji
+    'fin': 'fi',  # Finland
+    'fra': 'fr',  # France
+    'gab': 'ga',  # Gabon
+    'gmb': 'gm',  # Gambia
+    'geo': 'ge',  # Georgia
+    'deu': 'de',  # Germany
+    'gha': 'gh',  # Ghana
+    'grc': 'gr',  # Greece
+    'grd': 'gd',  # Grenada
+    'gtm': 'gt',  # Guatemala
+    'gin': 'gn',  # Guinea
+    'gnb': 'gw',  # Guinea-Bissau
+    'guy': 'gy',  # Guyana
+    'hti': 'ht',  # Haiti
+    'hnd': 'hn',  # Honduras
+    'hun': 'hu',  # Hungary
+    'isl': 'is',  # Iceland
+    'ind': 'in',  # India
+    'idn': 'id',  # Indonesia
+    'irn': 'ir',  # Iran
+    'irq': 'iq',  # Iraq
+    'irl': 'ie',  # Ireland
+    'isr': 'il',  # Israel
+    'ita': 'it',  # Italy
+    'jam': 'jm',  # Jamaica
+    'jpn': 'jp',  # Japan
+    'jor': 'jo',  # Jordan
+    'kaz': 'kz',  # Kazakhstan
+    'ken': 'ke',  # Kenya
+    'kir': 'ki',  # Kiribati
+    'prk': 'kp',  # North Korea
+    'kor': 'kr',  # South Korea
+    'kwt': 'kw',  # Kuwait
+    'kgz': 'kg',  # Kyrgyzstan
+    'lao': 'la',  # Laos
+    'lva': 'lv',  # Latvia
+    'lbn': 'lb',  # Lebanon
+    'lso': 'ls',  # Lesotho
+    'lbr': 'lr',  # Liberia
+    'lby': 'ly',  # Libya
+    'lie': 'li',  # Liechtenstein
+    'ltu': 'lt',  # Lithuania
+    'lux': 'lu',  # Luxembourg
+    'mdg': 'mg',  # Madagascar
+    'mwi': 'mw',  # Malawi
+    'mys': 'my',  # Malaysia
+    'mdv': 'mv',  # Maldives
+    'mli': 'ml',  # Mali
+    'mlt': 'mt',  # Malta
+    'mhl': 'mh',  # Marshall Islands
+    'mrt': 'mr',  # Mauritania
+    'mus': 'mu',  # Mauritius
+    'mex': 'mx',  # Mexico
+    'fsm': 'fm',  # Micronesia
+    'mda': 'md',  # Moldova
+    'mco': 'mc',  # Monaco
+    'mng': 'mn',  # Mongolia
+    'mne': 'me',  # Montenegro
+    'mar': 'ma',  # Morocco
+    'moz': 'mz',  # Mozambique
+    'mmr': 'mm',  # Myanmar (Burma)
+    'nam': 'na',  # Namibia
+    'nru': 'nr',  # Nauru
+    'npl': 'np',  # Nepal
+    'nld': 'nl',  # Netherlands
+    'nzl': 'nz',  # New Zealand
+    'nic': 'ni',  # Nicaragua
+    'ner': 'ne',  # Niger
+    'nga': 'ng',  # Nigeria
+    'mkd': 'mk',  # North Macedonia
+    'nor': 'no',  # Norway
+    'omn': 'om',  # Oman
+    'pak': 'pk',  # Pakistan
+    'plw': 'pw',  # Palau
+    'pan': 'pa',  # Panama
+    'png': 'pg',  # Papua New Guinea
+    'pry': 'py',  # Paraguay
+    'per': 'pe',  # Peru
+    'phl': 'ph',  # Philippines
+    'pol': 'pl',  # Poland
+    'prt': 'pt',  # Portugal
+    'qat': 'qa',  # Qatar
+    'rou': 'ro',  # Romania
+    'rus': 'ru',  # Russia
+    'rwa': 'rw',  # Rwanda
+    'kna': 'kn',  # Saint Kitts and Nevis
+    'lca': 'lc',  # Saint Lucia
+    'vct': 'vc',  # Saint Vincent and the Grenadines
+    'wsm': 'ws',  # Samoa
+    'smr': 'sm',  # San Marino
+    'stp': 'st',  # Sao Tome and Principe
+    'sau': 'sa',  # Saudi Arabia
+    'sen': 'sn',  # Senegal
+    'srb': 'rs',  # Serbia
+    'syc': 'sc',  # Seychelles
+    'sle': 'sl',  # Sierra Leone
+    'sgp': 'sg',  # Singapore
+    'svk': 'sk',  # Slovakia
+    'svn': 'si',  # Slovenia
+    'slb': 'sb',  # Solomon Islands
+    'som': 'so',  # Somalia
+    'zaf': 'za',  # South Africa
+    'ssd': 'ss',  # South Sudan
+    'esp': 'es',  # Spain
+    'lka': 'lk',  # Sri Lanka
+    'sdp': 'sd',  # Sudan
+    'sur': 'sr',  # Suriname
+    'swe': 'se',  # Sweden
+    'che': 'ch',  # Switzerland
+    'syr': 'sy',  # Syria
+    'tjk': 'tj',  # Tajikistan
+    'tza': 'tz',  # Tanzania
+    'tha': 'th',  # Thailand
+    'tls': 'tl',  # Timor-Leste
+    'tgo': 'tg',  # Togo
+    'ton': 'to',  # Tonga
+    'tto': 'tt',  # Trinidad and Tobago
+    'tun': 'tn',  # Tunisia
+    'tur': 'tr',  # Turkey
+    'tkm': 'tm',  # Turkmenistan
+    'tuv': 'tv',  # Tuvalu
+    'uga': 'ug',  # Uganda
+    'ukr': 'ua',  # Ukraine
+    'are': 'ae',  # United Arab Emirates
+    'gbr': 'gb',  # United Kingdom
+    'usa': 'us',  # United States of America
+    'ury': 'uy',  # Uruguay
+    'uzb': 'uz',  # Uzbekistan
+    'vut': 'vu',  # Vanuatu
+    'vat': 'va',  # Vatican City
+    'ven': 've',  # Venezuela
+    'vnm': 'vn',  # Vietnam
+    'yem': 'ye',  # Yemen
+    'zmb': 'zm',  # Zambia
+    'zwe': 'zw'   # Zimbabwe
+}
